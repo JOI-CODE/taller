6a0b6485fa6bfdf5bbf764ffc479cb52b452a13d
JOI-CODE <luisborrero885@gmail.com>
2024-09-18 18:28:12 -0700

    joisertaller

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <h1>pokemon</h1>
+     <img class="imagen-logo" src="C:\Users\Admin\Desktop\Taller\taller\src\assets\logo_pokemon.png" alt="">
+ </div>
+
+
+      <div class="mosaicos" id="div-imagenes" >
+         
+       </div>
+  
+<!--  -->
+
+  
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    methods: {
+        cargarImagenes: async function () {
+            let url = 'https://cobuses.com.co/APIV2/model/pokemon.php?dato=getallpokemon';
+
+            await this.axios.get(url)
+                .then(response => {
+                    // Manejar la respuesta exitosa
+                    let ImagesResponse = response.data;
+                    const div_imagenes = document.getElementById('div-imagenes');
+                    ImagesResponse.forEach(image => {
+                        const div_imagen = document.createElement("div");
+                        console.log(image.image)
+                        div_imagen.innerHTML = `<div class="joi">`+`
+                            <img src="${image.imagen}"/>
+
+
+                        `+`<h1>${image.nombre}</h1>`+
+                        +`<h1 style="">${image.type_name}</h1>`+`</div>`
+                        div_imagenes.appendChild(div_imagen)
+                    });
+                })
+                .catch(error => {
+                    
+                    console.error(error);
+                });
+        }
+    },
+    mounted() {
+        this.cargarImagenes();
+    }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.header {
+  color: antiquewhite;
+    background: #000;
+    height: 80px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
+/* .imagen-logo {
+    height: 100%;
+    object-fit: cover;
+    object-position: center center;
+} */
+.mosaicos {
+    width: 80%;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    border: 3px;
+    background-color: burlywood ;
+    }
 </style>
